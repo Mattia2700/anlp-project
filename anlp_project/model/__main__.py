c07eb63d55e5896c451ec35a53f4bb96653a9ac3
Mattia <mattia2700franzin@gmail.com>
2024-06-16 13:48:54 +0000

    feat: training ready

--- anlp_project/model/__main__.py
+++ anlp_project/model/__main__.py
@@ -1,14 +1,13 @@
 import os
 
 import torch
-import wandb
 from pytorch_lightning import Trainer
 from pytorch_lightning.callbacks import StochasticWeightAveraging, LearningRateFinder
 from pytorch_lightning.loggers import WandbLogger
-from torch.utils.data import DataLoader
 
 from anlp_project.model.dataset import MELDText
 from anlp_project.model.model import LyricsClassifier
+from pytorch_lightning.tuner import Tuner
 
 
 class FineTuneLearningRateFinder(LearningRateFinder):
@@ -24,58 +23,17 @@ class FineTuneLearningRateFinder(LearningRateFinder):
             self.lr_find(trainer, pl_module)
 
 
-def collate_fn(batch):
-    x, y = zip(*batch)
-    # get the max length of the input_ids
-    max_len = max([len(i["input_ids"]) for i in x])
-    # pad the input_ids and attention_mask
-    for i in range(len(x)):
-        x[i]["input_ids"] = x[i]["input_ids"].tolist() + [0] * (
-            max_len - len(x[i]["input_ids"])
-        )
-        x[i]["attention_mask"] = x[i]["attention_mask"].tolist() + [0] * (
-            max_len - len(x[i]["attention_mask"])
-        )
-    x = {k: torch.tensor([i[k] for i in x]) for k in x[0].keys()}
-    y = torch.stack(y)
-    return x, y
-
-
 def main():
-    wandb.finish()
-    model_name, lr, num_labels = ("FacebookAI/roberta-base", 1e-3, 7)
-    model = LyricsClassifier(model_name, lr, num_labels)
-    batch_size = 32
-    train_data = MELDText("train", model_name)
-    train_data = DataLoader(
-        train_data,
-        batch_size=batch_size,
-        shuffle=True,
-        collate_fn=collate_fn,
-        num_workers=os.cpu_count(),
-    )
-    val_data = MELDText("dev", model_name)
-    val_data = DataLoader(
-        val_data,
-        batch_size=batch_size,
-        collate_fn=collate_fn,
-        num_workers=os.cpu_count(),
-    )
-    test_data = MELDText("test", model_name)
-    test_data = DataLoader(
-        test_data,
-        batch_size=batch_size,
-        collate_fn=collate_fn,
-        num_workers=os.cpu_count(),
-    )
+    model_name, lr, num_labels, batch_size = ("FacebookAI/roberta-base", 1e-3, 7, 64)
+    model = LyricsClassifier(model_name, lr, num_labels, batch_size)
+
+    epochs = 10
 
     logger = WandbLogger(
         project="anlp-project",
         name=f"{model_name.split('/')[1]}-{batch_size}-{lr}-{num_labels}",
     )
     logger.experiment.config["batch_size"] = batch_size
-
-    epochs = 10
 
     trainer = Trainer(
         max_epochs=epochs,
@@ -86,5 +44,8 @@ def main():
         ],
     )
 
-    trainer.fit(model, train_data, val_data)
-    trainer.test(model, test_data)
+    tuner = Tuner(trainer)
+    tuner.scale_batch_size(model, mode='binsearch')
+
+    trainer.fit(model, model.train_dataloader(), model.val_dataloader())
+    trainer.test(model, model.test_dataloader())
--- anlp_project/model/model.py
+++ anlp_project/model/model.py
@@ -1,14 +1,18 @@
 import torch
+import os
 from pytorch_lightning import LightningModule
 from transformers import AutoModelForSequenceClassification
+from torch.utils.data import DataLoader
+from anlp_project.model.dataset import MELDText
 
 
 class LyricsClassifier(LightningModule):
-    def __init__(self, model_name, lr, num_labels):
+    def __init__(self, model_name, lr, num_labels, batch_size):
         super().__init__()
         self.model_name = model_name
         self.lr = lr
         self.num_labels = num_labels
+        self.batch_size = batch_size
 
         self.model = AutoModelForSequenceClassification.from_pretrained(
             self.model_name,
@@ -22,7 +26,7 @@ class LyricsClassifier(LightningModule):
         return x
 
     def configure_optimizers(self):
-        optimizer = torch.optim.Adam(self.parameters(), lr=self.lr)
+        optimizer = torch.optim.AdamW(self.parameters(), lr=self.lr)
         return optimizer
 
     def training_step(self, batch, batch_idx):
@@ -45,3 +49,48 @@ class LyricsClassifier(LightningModule):
         loss = outputs.loss
         self.log("test_loss", loss)
         return loss
+
+    def train_dataloader(self):
+        train_data = MELDText("train", self.model_name)
+        return DataLoader(
+            train_data,
+            batch_size=self.batch_size,
+            shuffle=True,
+            collate_fn=self.collate_fn,
+            num_workers=os.cpu_count(),
+        )
+
+    def val_dataloader(self):
+        val_data = MELDText("dev", self.model_name)
+        return DataLoader(
+            val_data,
+            batch_size=self.batch_size,
+            collate_fn=self.collate_fn,
+            num_workers=os.cpu_count(),
+        )
+    
+    def val_dataloader(self):
+        test_data = MELDText("test", self.model_name)
+        return DataLoader(
+            test_data,
+            batch_size=self.batch_size,
+            collate_fn=self.collate_fn,
+            num_workers=os.cpu_count(),
+        )
+
+    @staticmethod
+    def collate_fn(batch):
+        x, y = zip(*batch)
+        # get the max length of the input_ids
+        max_len = max([len(i["input_ids"]) for i in x])
+        # pad the input_ids and acollate_fn,ttention_mask
+        for i in range(len(x)):
+            x[i]["input_ids"] = x[i]["input_ids"].tolist() + [0] * (
+                max_len - len(x[i]["input_ids"])
+            )
+            x[i]["attention_mask"] = x[i]["attention_mask"].tolist() + [0] * (
+                max_len - len(x[i]["attention_mask"])
+            )
+        x = {k: torch.tensor([i[k] for i in x]) for k in x[0].keys()}
+        y = torch.stack(y)
+        return x, y
